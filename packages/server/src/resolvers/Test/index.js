c6e322582cc524dfd4ec43a123b9b49d28af2ad9
Gloria Solomon <37818301+RwandaGloria@users.noreply.github.com>
2023-05-05 09:19:08 +0100

    Delete index.js

--- packages/server/src/resolvers/Test/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.testResolvers = void 0;
-exports.testResolvers = {
-    Query: {
-        hello: function () { return 'Hello world!'; },
-    },
-};
