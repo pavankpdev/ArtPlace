b52a9feb52078d13ff197104301e05531af57aea
Gloria Solomon <37818301+RwandaGloria@users.noreply.github.com>
2023-05-05 09:17:16 +0100

    Delete index.js

--- packages/server/src/resolvers/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.resolvers = void 0;
-var merge_1 = require("@graphql-tools/merge");
-var Test_1 = require("./Test");
-exports.resolvers = (0, merge_1.mergeResolvers)([Test_1.testResolvers]);
