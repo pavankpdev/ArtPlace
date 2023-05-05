4582ff29c15da5f3bc12846a95345dbb3098beb1
Gloria Solomon <37818301+RwandaGloria@users.noreply.github.com>
2023-05-05 09:14:18 +0100

    Delete index.js

--- packages/server/src/schema/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.typeDefs = void 0;
-var merge_1 = require("@graphql-tools/merge");
-var Test_1 = require("./Test");
-exports.typeDefs = (0, merge_1.mergeTypeDefs)([Test_1.testTypeDefs]);
